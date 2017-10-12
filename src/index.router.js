60e857267070ddb0dca244fa2af22e97b2f63f39
Chris Chen <wasi.chris@gmail.com>
2017-10-13 00:21:39 +0800

    使用 BrowserRouter 不需要 history

--- src/index.router.js
+++ src/index.router.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { hashHistory, Switch } from 'react-router'
+import { Switch } from 'react-router'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 
 // impot routes
 import {Home, NotFound} from 'routes'
 
 const MainRouter = () => (
-  <Router key={Math.random()} history={hashHistory}>
+  <Router key={Math.random()} >
     <Switch>
       <Route path='/' exact component={Home} />
       <Route component={NotFound} />
